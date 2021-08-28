589c8950607d46addc77812da14d11d9536158af
sunil kumar maurya <sunilkumar.maurya@team.telstra.com>
2021-08-28 08:06:48 +0530

    added constructor and setter Dependency Injection

---
+++ sfg-di/src/test/java/guru/springframework/sfgdi/controllers/PropertyInjectedControllerTest.java
@@ -0,0 +1,23 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class PropertyInjectedControllerTest {
+
+    PropertyInjectedController propertyInjectedController;
+
+    @BeforeEach
+    void setUp() {
+        propertyInjectedController = new PropertyInjectedController();
+        propertyInjectedController.greetingService = new GreetingServiceImpl();
+    }
+
+    @Test
+    public void getGreeting(){
+        System.out.println(propertyInjectedController.getGreeting());
+    }
+
+}
